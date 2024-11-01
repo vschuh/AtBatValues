992acf86a97bd4fcbc9b12f9137487c1942b2fcf
Schuh Valentin <56673098+vschuh@users.noreply.github.com>
2024-11-01 21:20:17 +0100

    Add files via upload

---
+++ SBLR.sql
@@ -0,0 +1,16 @@
+SELECT chances.throws "Throws",chances "Chances", attempts "Attempts", concat(((attempts::double precision/chances)*100)::numeric(4,2),'%') "Attempt%" FROM 
+	(SELECT pr.throws,count(*) as chances from gameevents
+		join game on game.id = gameid
+		join player_record pr on pitcherid = pr.id
+	    where (runner1 > 0 and runner2 = 0) and (pr.throws = 'L' or  pr.throws = 'R')
+	    and gameevents.pa = 1 and game.tournamentid in (SELECT id FROM tournamentevent where tier = 1 
+		and federation_id in (SELECT id from federation where tier = 1) and sport <5) 
+		group by pr.throws) as chances,
+	(SELECT pr.throws,count(*) attempts from basestealing 
+		join player_record pr on pitcherid = pr.id
+		join game on game.id = gameid
+		where (pr.throws = 'L' or  pr.throws = 'R') and base = 2 and game.tournamentid in (SELECT id FROM tournamentevent where tier = 1
+		and federation_id in (SELECT id from federation where tier = 1) and sport <5)
+	group by pr.throws) as attempts
+	where chances.throws = attempts.throws
+	
